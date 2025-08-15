35fe34ca1121a8bf63cd0da898d55ba2bed85797
barbariska111 <m89538784022@gmail.com>
2025-08-16 00:42:09 +0700

    add news

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def credits(request):
     content = "Nicky\nMama"
     return HttpResponse(content, content_type="text/plain")
 
+def news(request):
+    data = {"news": [
+        "mama now has a news page!",
+        "mama has its first page"
+    ]}
+    return render(request, "news.html", data)
