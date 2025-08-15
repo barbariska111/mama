0e320acb5f8e9c14b9568c94e6755054a7c7887f
barbariska111 <m89538784022@gmail.com>
2025-08-15 23:22:00 +0700

    credits page

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def credits(request):
+    content = "Nicky\nMama"
+    return HttpResponse(content, content_type="text/plain")
+
